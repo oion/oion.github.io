c9e0c0fefbd5ad151c7500859b4ea3abd25ef483
fingerontheweb <fingerontheweb@yahoo.com>
2018-04-16 07:11:05 -0400

    WIP

--- js/main.js
+++ js/main.js
@@ -1,10 +1,35 @@
 $(function() {
-    
-	$("#bot").typed({
-        stringsElement: $('#lines'),
-        typeSpeed: 0,
-        loop: true,
-        backDelay: 1000,
-    });
+  
+	$( document ).ready(function() {
+		
+		greeting();
+		typingAnimation();
+		
+    function greeting() {
+				var today = new Date()
+				var curHr = today.getHours()
+				var greeting = "Hello.";
 
+				if (curHr < 12) {
+					greeting = 'Good Morning.';
+				} else if (curHr < 18) {
+					greeting = 'Good Afternoon.';
+				} else {
+					greeting = 'Good Evening.';
+				}
+				$('#greeting').html(greeting);
+				console.log($('#greeting').html());	
+		}
+		
+		function typingAnimation() {
+			$("#bot").typed({
+				stringsElement: $('#lines'),
+				typeSpeed: 0,
+				loop: true,
+				backDelay: 1000,
+			});
+		}
+		
+	});
+	
 });
